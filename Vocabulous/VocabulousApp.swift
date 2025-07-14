600f2dac3c1b10c7b98b3a96269cbb12d5c5b8dd
Jake Ashton <jakemaddenashton@gmail.com>
2025-07-13 17:05:07 -0700

    Initial Commit

---
+++ Vocabulous/VocabulousApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabulousApp.swift
+//  Vocabulous
+//
+//  Created by Jake Ashton on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct VocabulousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocabulousTests/VocabulousTests.swift
@@ -0,0 +1,17 @@
+//
+//  VocabulousTests.swift
+//  VocabulousTests
+//
+//  Created by Jake Ashton on 7/13/25.
+//
+
+import Testing
+@testable import Vocabulous
+
+struct VocabulousTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
