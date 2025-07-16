e742ded46ee5e74b173a98b22cb3fdbfd030fc3c
Jake Ashton <jakemaddenashton@gmail.com>
2025-07-15 21:26:00 -0700

    feat: add dictionary file and loading logic/types

---
+++ Vocabulous/Models/Entry.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Entry: Hashable, Codable, Identifiable {
+    let id: Int
+    let lemma: String
+    let enlemma: String
+    let clar: String?
+    let pos: String
+    let gender: String?
+    let tense: String
+    let notes: String?
+}
