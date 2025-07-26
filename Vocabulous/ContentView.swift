9e78db650f213a32f2e236b2fe0d7a1a90c8bb12
Jake Ashton <jakemaddenashton@gmail.com>
2025-07-26 12:10:28 -0700

    feat: add back/forward buttons and initial word history logic

--- Vocabulous/ContentView.swift
+++ Vocabulous/ContentView.swift
@@ -3,25 +3,55 @@ import SwiftUI
 struct ContentView: View {
     @State private var modelData = ModelData()
     @State private var currentWord: Entry
+    @State private var allWords: [Entry]
 
     init() {
         let model = ModelData()
+        let firstWord = model.words.randomElement()!
+        
         _modelData = State(initialValue: model)
-        _currentWord = State(initialValue: model.words.randomElement()!)
+        _currentWord = State(initialValue: firstWord)
+        _allWords = State(initialValue: [firstWord])
     }
 
     var body: some View {
-        VStack(spacing: 40) {
-            WordDetail(word: currentWord)
-
-            Button("Show Another Word") {
+        HStack {
+            if allWords.count > 1 {
+                Button {
                 if let newWord = modelData.words.randomElement() {
                     currentWord = newWord
+                    allWords.append(newWord)
                 }
             }
-            .buttonStyle(.borderedProminent)
-        }
+                label: {
+                    Label("Go to previous word", systemImage: "chevron.backward")
+                        .labelStyle(.iconOnly)
+                        .font(.system(size: 30))
+                        .foregroundColor(.secondary)
+                }
+            }
+            
+            Spacer()
+            
+            WordDetail(word: currentWord)
+            
+            Spacer()
+
+            Button {
+                if let newWord = modelData.words.randomElement() {
+                    currentWord = newWord
+                    allWords.append(newWord)
+                }
+            }
+                label: {
+                    Label("Go to next word", systemImage: "chevron.forward")
+                        .labelStyle(.iconOnly)
+                        .font(.system(size: 30))
+                        .foregroundColor(.secondary)
+                }
+            }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(20)
     }
 }
 
